e3706781d07c9ae5f4e44d0c446f048542d78bb0
Cameo-Chang-29 <lich.xiao@gmail.com>
2014-07-17 14:15:50 -0400

    some modifications of Match.java

--- quiz2/src/org/adastraeducation/quiz/Match.java
+++ quiz2/src/org/adastraeducation/quiz/Match.java
@@ -4,10 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 /**
- * Match 
+ * Match Question is kind of question that to choose the right answer to match what the question describes.
  * 
- * @author qiangzhang
- * date 6/30/2014 
+ * @author Chang Li
+ * created by date 7/15/2014 
+ * 1st modified 7/17/2014
  */
 
 public class Match extends Question {
@@ -37,10 +38,10 @@ public class Match extends Question {
 		Match m1 = new Match("Match Question 1", "1", 
 				"choose the right answer to match the picture on the left", "f",
 				questions1,"t", ans1, "f"); 
-		String[] questions2 = {"tyrannosaurus", "shark", "alligator", 
-				"crocodile", "apatosaurus", "guppy"};
+		String[] questions2 = {"Tyrannosaurus", "Shark", "Alligator", 
+				"Crocodile", "Apatosaurus", "Guppy"};
 		Match m2 = new Match("Match Question 2", "1", 
-				"choose the right answer to match the picture on the left", "f",
+				"choose the right answer to match the description on the left", "f",
 				questions2, "f", ans1, "f");
 		quiz.addQuestion(m1);
 		quiz.addQuestion(m2);
@@ -49,13 +50,13 @@ public class Match extends Question {
 	public void writeHTMLContent(StringBuilder b) {
 		b.append("<ol type=\"A\">");
 		for (int i = 0; i < this.questions.length; i++) {
-			b.append("<li/><br/>");
+			b.append("<li/>");
 
 			if (imgQuestions)
-				b.append("<img src=\"../img/").append(questions[i]).append("width 200 height 140")
-				.append("\"/>");
+				b.append("<br/><img src=\"../img/temp").append(questions[i]).append("width=\"200\" height=\"140\"")
+				.append("\"/>");//modification
 			else 
-				b.append(questions[i]);//TODO:add html
+				b.append("<p1/>").append(questions[i]);//modification
 			
 			b.append("<select name=\"").append(getName()).
 			append("\">");
@@ -63,8 +64,8 @@ public class Match extends Question {
 				if (imgAnswers)
 					b.append("");//TODO:if the answers are pictures.
 				else {
-					b.append("<option value=\"  \">").append(answers[j])
-					.append("</option>");//TODO:value?					
+					b.append("<option value=\"").append(j).append("\">").append(answers[j])//modificaiton
+					.append("</option>");				
 				}		
 			}
 			b.append("</select>");
@@ -80,7 +81,6 @@ public class Match extends Question {
 		for(int j = 0; j < questions.length ; j++)
 			b.append("Q m=\" \">").append(questions[j]).append("/Q");
 		for(int i = 0; i < answers.length ; i++) 
-			b.append("A m=\" \">").append(answers[i]).append("/A");		
+			b.append("A m=\" \">").append(answers[i]).append("/A");	//TODO: the value of m?	
 	}
 }
-
