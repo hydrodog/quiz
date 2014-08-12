8a4b26e7049941ba392791691b65e120bc9ade31
Czhang0727 <czhang0727@gmail.com>
2014-08-12 15:38:29 -0400

    BOOTSTRAP & MultiAnswer DB fixed

--- quiz2/src/org/adastraeducation/quiz/MultiAnswer.java
+++ quiz2/src/org/adastraeducation/quiz/MultiAnswer.java
@@ -3,56 +3,136 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.adastraeducation.quiz;
+package cpe642;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author zhangchenyi
  */
-public class MultiAnswer extends Question {
-    private final MultiAnswer_Answer answers[];
+public final class MultiAnswer extends Question{
     
-    public MultiAnswer(String title, String question, String[] answers, String imgAnswer) {
-    	super(title, "1", question, false);
+    private final int num_selection;
+    private final int level = 0;
+    private final String id;
+    private final String name;
+    private final MultiAnswer_Answer Answers[];
+    private final String desc;
+    
+    
+    MultiAnswer(int num_selection, String id, String name, String desc, String Answer[], String Image[],boolean results[]) throws SQLException
+    {
+         this.num_selection = num_selection;
+         this.name = name;
+         this.id = id;
+         this.desc = desc;
          
-         this.answers = new MultiAnswer_Answer[answers.length / 2];
-         for (int i = 0; i < answers.length; i += 2) {
-             this.answers[i/2] = new MultiAnswer_Answer(answers[i], answers[i+1].equals("t"));
+         this.Answers = new MultiAnswer_Answer[this.num_selection];
+         for(int i =0;i<this.num_selection;i++)
+         {
+             this.Answers[i] = new MultiAnswer_Answer(Answer[i],Image[i],results[i]);
          } 
+         
+         update_database();
     }
 
- 
-	@Override
-	public boolean isCorrect(String[] ans) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public double gradeIt(String[] answers) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getTagName() {
-		// TODO Auto-generated method stub
-		return "MultiAnswer";
-	}
-
-	@Override
-	public void writeHTMLContent(StringBuilder b) {
-	   for (int i = 0; i < answers.length; i++) {
-           b.append("<input type = \"checkbox\"name = \"")
-           .append(getName()).append(i).append("\" ")
-           .append(this.answers[i].textanswer());
-       }
-	}
-
-	@Override
-	public void writeXMLContent(StringBuilder b) {
-		endTagWriteQuestion(b);
-        for (int i = 0; i < answers.length; i++) {
-        	answers[i].writeXMLContent(b);
+    public void update_database() throws SQLException
+    {
+         String sql = "INSERT INTO MULTIANSWER"
+				+ "(LEVEL, NAME, DESCRIPTION, ID) " + "VALUES (\""
+				+ level+"\",\""+name+"\",\""+desc+"\",\""+id+"\")";
+         Mysql_Connector con = new Mysql_Connector();
+         con.single_process(sql);
+         
+        int P_ID = 0;
+        
+        sql = "SELECT * FROM MultiAnswer;";
+        ResultSet res;
+        res = con.where(sql,"P_ID");
+        while(res.next())
+                       {
+                           P_ID = res.getInt("p_id");
+                       }
+        
+        System.out.println(P_ID);
+        for (int i =0;i<Answers.length;i++) 
+        {
+            sql = "INSERT INTO MULTIANSWER_ANSWER"
+				+ "(P_ID, A_CORRECT,A_CONTENT,A_IMG) " + "VALUES (\""
+				+ P_ID +"\",\""
+                                +Answers[i].Result+"\",\""
+                                +Answers[i].Description+"\",\""
+                                +Answers[i].Image+"\")";
         }
-	}
+    }
+    
+    public void writeHTML(StringBuilder b) {
+        String result = "";
+    
+        
+        result = this.desc +"\n<br/>";
+        for(int i =0;i<this.num_selection;i++)
+            {
+                result = result + "<input type = \"checkbox\"name = \""+this.name+i+"\" ";
+                result = result + this.Answers[i].get_answer_HTML();
+            }
+        
+        
+        b.append(result);
+    }
+
+    @Override
+    public void writeXML(StringBuilder b) {
+        String result;
+        
+        result = "<MultiAnswer id=\""+this.id+"\" name=\""+this.name+"\" level=\"1\">\n"
+                    +this.desc+"\n";
+        
+        for(int i =0;i<this.num_selection;i++)
+        {
+            result = result + Answers[i].get_answer_XML();
+        }
+        
+        result = result + "</MultiAnswer>";
+        b.append(result);
+    }
+    
+    public void testXMLAndHTML() throws SQLException
+    {
+        MultiAnswer test;
+        
+        String answer[] = new String [4];
+        answer[0] = "T_Answer1";
+        answer[1] = "T_Answer2";
+        answer[2] = "T_Answer3";
+        answer[3] = "T_Answer4";
+        
+        String img[] = new String [4];
+        img[0] = "1.jpg";
+        img[1] = "2.jpg";
+        img[2] = "3.jpg";
+        img[3] = "4.jpg";
+        
+        boolean result[] = new boolean [4];
+        result[0] = true;
+        result[1] = false;
+        result[2] = false;
+        result[3] = true;
+        
+        String desc = "This is a Sample Problem!!";
+        String id = "ID";
+        String name = "name";
+        
+        
+        test = new MultiAnswer(4,id,name,desc,answer,img,result);
+        
+        StringBuilder html = new StringBuilder();
+        StringBuilder xml = new StringBuilder();
+        
+        test.writeHTML(html);
+        test.writeXML(xml);
+    }
 }
--- quiz2/src/org/adastraeducation/quiz/MultiAnswer_Answer.java
+++ quiz2/src/org/adastraeducation/quiz/MultiAnswer_Answer.java
@@ -4,31 +4,69 @@
  * and open the template in the editor.
  */
 
-package org.adastraeducation.quiz;
+package cpe642;
 /**
  *
  * @author zhangchenyi
  */
-public class MultiAnswer_Answer extends Answer {
-    MultiAnswer_Answer(String answer, boolean isCorrect) {
-    	super(answer, isCorrect);
-    }
+public class MultiAnswer_Answer extends Answer{
+        
+        String Description;
+        String Image;
+        boolean Result;
+    
+        MultiAnswer_Answer(String Description, String Image, boolean Result)
+        {
+            this.Description = Description;
+            this.Image = Image;
+            this.Result = Result;
+        }
 
-    MultiAnswer_Answer() {}
-
-    public String textanswer() {
-    	return "<A correct = \"" + this.getCorrect() + "\">" +
-    			this.getAnswer() + "</A>\n";
-//    	return "<A correct = \"" + this.getCorrect() + "\">" +
-//		this.getAnswer() + "</A>" + "<img src = \""+this.Image+"\"/>\n";
+    MultiAnswer_Answer() {
+        throw new UnsupportedOperationException("Lost Arguments!!"); //To change body of generated methods, choose Tools | Templates.
     }
-
-    public String graphanswer() {
-    	return "value =\"true\">" + "<img src = \"" + 
-    			this.getAnswer() + "\"/><br/>" + "\n";
-    }
-    public void writeXMLContent(StringBuilder b) {
-    	b.append("<A correct = \"" + getCorrect() + "\">")
-    		.append(getAnswer()).append("</A>");
-    }
+        
+        public String get_answer_XML()
+        {
+            String result = "";
+            
+            result = "<A correct = \"" + this.Result + "\">" + this.Description + "<\\A>" + "<img src = \""+this.Image+"\"/>\n";
+            
+            return result;
+        }
+        
+        public boolean insert()
+        {
+            
+            return true;
+        }
+        
+        public String get_answer_HTML()
+        {
+            String result = "";
+            
+            result = "value =\"true\">"+this.Description+"<img src = \""+this.Image+"\"/><br/>"+"\n";
+            
+            return result;
+        }
+        
+        public boolean Answer_Check(boolean check)
+        {
+            if(this.Result == check)
+            return true;
+            else return false;
+        }
+    /**
+     *
+     * @return
+     */
+        public boolean get_result()
+        {
+            return Result;
+        }
+        
+        public String get_desc()
+        {
+            return Description;
+        } 
 }
